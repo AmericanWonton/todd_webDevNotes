551a8dea17e1fa5ac9d820e141903d5482016077
AmericanWonton <jbkeller0303@gmail.com>
2020-03-25 15:48:53 -0500

    Did another lesson, now we're in state.

---
+++ creating_state/01_url/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	v := req.FormValue("q")
+	fmt.Fprintln(w, "Do my search: "+v)
+}
+
+// visit this page:
+// http://localhost:8080/?q=dog
+
+/* In this example, the query variable q is assigned the value of dog */
+/* Try removing everything past 8080 and see what happens! */
---
+++ creating_state/02_form-post/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	v := req.FormValue("q")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `
+	<form method="POST">
+	 <input type="text" name="q">
+	 <input type="submit">
+	</form>
+	<br>`+v)
+}
+
+/* Remember, since those are getting sent in a POST, it goes through the body, not the URL
+If you use GET, it's through the URL  This is example 02 and 03*/
