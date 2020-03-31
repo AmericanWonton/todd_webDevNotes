91b10893be5c16e8b6f7256ee82eb605aa5df257
AmericanWonton <jbkeller0303@gmail.com>
2020-03-30 19:28:15 -0500

    Still working on state.

--- creating_state/05_form-file/02_store/main.go
+++ creating_state/05_form-file/02_store/main.go
@@ -43,23 +43,27 @@ func foo(w http.ResponseWriter, req *http.Request) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		s = string(bs)
+		s = string(bs) //We turn the string into a byte slice
 
 		// store on server
+		/* Here we get the filename from the header and create the file, then put it in that user folder */
 		dst, err := os.Create(filepath.Join("./user/", h.Filename))
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		defer dst.Close()
-
+		/* Here we write the contents of the file to the byte slice */
 		_, err = dst.Write(bs)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	}
-
+	/* Here we write the results of the byte slice to the file */
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	tpl.ExecuteTemplate(w, "index.gohtml", s)
 }
+
+/* This does the same as the last example but reads the file as well.
+I suggest using a text file to be simple */
